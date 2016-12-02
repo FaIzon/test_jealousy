bac157695ae899cf5b8610168a7c81081569fb7d
FaIzon <dimer90@gmail.com>
2016-12-02 21:24:21 +0700

    Delete empty stings

--- main.rb
+++ main.rb
@@ -6,8 +6,7 @@ puts "Тест ревнивы ли Вы. Версия 0.1\n\n"
 test = Test.new
 result_printer = ResultPrinter.new
 
-while test.next_question == true do
-
+while test.next_question  do
 end
 
 result_printer.print_result(test)
